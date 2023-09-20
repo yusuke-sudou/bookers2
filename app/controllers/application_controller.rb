fcbe2b24abd6da502c471b1325f2bd772b7fe278
EC2 Default User <ec2-user@ip-172-31-40-197.ap-northeast-1.compute.internal>
2023-09-20 09:11:22 +0000

    [Add]CRUD実装まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    user_path(@user.id)
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: "about"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   resources :books, only: [:new, :index, :show, :edit, :create, :destroy, :update]
-  resources :users, only: [:show, :edit, :index]
-  
+  resources :users, only: [:show, :edit, :index, :update]
+
 end
